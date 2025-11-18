33211cfa54497cc0ff370ba8ab731ac3d6dbe32a
Assolita <118940874+Assolita@users.noreply.github.com>
2025-11-18 20:20:42 -0300

    Update cadastro.js

--- public/src/js/cadastro.js
+++ public/src/js/cadastro.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
             try {
                 // 3. Faz o 'fetch' para o novo endpoint
                 
-                const response = await fetch(`/api/cadastro`, {
+                const response = await fetch(`/cadastro`, {
                   method: 'POST',
                   headers: { 'Content-Type': 'application/json' },
                   body: JSON.stringify({ username, email, password })
